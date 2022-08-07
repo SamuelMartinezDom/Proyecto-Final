bb8221e829ff2861ef64e53d88bd2c5281bba1af
Samuel Martinez <samuelmz979arg@gmail.com>
2022-08-07 19:49:51 -0300

    Creación de los models y chequeo de base de datos

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post (models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    class Meta: ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('blog:blog_detail', args=[self.slug])
+    
