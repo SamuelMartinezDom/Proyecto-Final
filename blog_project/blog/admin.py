48f62b7883f295bfb50ecbaa93f04fcf620fc399
Samuel Martinez <samuelmz979arg@gmail.com>
2022-08-07 21:03:53 -0300

    Creacion de los posts en admin

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Post
+from blog.models import Post
+
+admin.site.register(Post)
--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -3,17 +3,19 @@ from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-class Post (models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
 
-    class Meta: ordering = ('-publish',)
+    class Meta:
+        ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+        
     def get_absolute_url(self):
         return reverse('blog:blog_detail', args=[self.slug])
     
