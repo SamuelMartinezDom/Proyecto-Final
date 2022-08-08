d609499e40c803a0087e8d78999d0704666fc969
Samuel Martinez <samuelmz979arg@gmail.com>
2022-08-07 21:11:13 -0300

    Creación de las views

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from blog.models import Post
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/blog_list.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/blog_detail.html'
