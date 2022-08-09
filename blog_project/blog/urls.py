6d1af0369ad6ff4e528de853b670d7645ee3f635
Samuel Martinez <samuelmz979arg@gmail.com>
2022-08-09 20:18:20 -0300

    Creacion de forms y creacion de articulos nuevos.

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,7 +1,7 @@
 #blog/urls.py
 
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from blog.views import BlogListView, BlogDetailView, new_blog
 
 app_name = 'blog'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     #post views
     path('', BlogListView.as_view(), name='blog_list'),
     path('<slug:slug>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('new-blog/', new_blog, name='new_blog')
 ]
